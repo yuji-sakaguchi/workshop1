5642394c85d606f005b9cb399fe2ea339fc21c9e
Yuji Sakaguchi <yujisakaguchi17@gmail.com>
2021-10-17 13:39:10 -0700

    React app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Get free food!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.duffl.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          duffl
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+function Recipe() {
+    return(<h1>Recipe Componenet</h1>)
+}
+
+export default Recipe
